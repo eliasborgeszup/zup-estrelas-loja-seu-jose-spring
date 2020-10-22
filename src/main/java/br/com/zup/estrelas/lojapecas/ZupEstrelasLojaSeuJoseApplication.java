b890e48f8881c45480a21a287e403463dbb47d8a
joaoalveszup <joao.alves@zup.com.br>
2020-10-22 12:35:41 -0300

    Initial commit

---
+++ src/main/java/br/com/zup/estrelas/lojapecas/ZupEstrelasLojaSeuJoseApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.estrelas.lojapecas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupEstrelasLojaSeuJoseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupEstrelasLojaSeuJoseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/estrelas/lojapecas/ZupEstrelasLojaSeuJoseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.estrelas.lojapecas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupEstrelasLojaSeuJoseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
